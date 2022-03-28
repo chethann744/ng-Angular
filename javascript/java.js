d39c700dedac7cfd2ddc0ee7aa3cffbfef5daba6
chethan <chethann744@gmail.com>
2022-03-28 23:14:21 +0530

    adding registration page

--- javascript/java.js
+++ javascript/java.js
@@ -1,71 +1,127 @@
-// let fullname= "chethan nagaraju"
-// console.log(fullname)
-// console.log(typeof fullname)
+// // let fullname= "chethan nagaraju"
+// // console.log(fullname)
+// // console.log(typeof fullname)
 
 
 
-// let bacthNumber = 99; //number datatype
-// console.log(bacthNumber);
-// console.log(typeof bacthNumber); //number
+// // let bacthNumber = 99; //number datatype
+// // console.log(bacthNumber);
+// // console.log(typeof bacthNumber); //number
 
-// bacthNumber = 'nine nine'; // updating value of already declared veriable 
-// console.log(bacthNumber); // 
-// console.log(typeof bacthNumber); // sting 
-
-
-
-// let batchSpeclization = 'MEAN/MERN';
-// console.log(batchSpeclization);
-// console.log(typeof batchSpeclization);
-
-// let allPresent = true; //block scope
-// console.log(allPresent); 
-// console.log(typeof allPresent);
-
-// var isLegacy = 'Yes'; 
-// console.log(isLegacy); // veriable with var get access thorught window object 
-// console.log(typeof isLegacy); //variable will availabe gloablly in program
+// // bacthNumber = 'nine nine'; // updating value of already declared veriable 
+// // console.log(bacthNumber); // 
+// // console.log(typeof bacthNumber); // sting 
 
 
 
+// // let batchSpeclization = 'MEAN/MERN';
+// // console.log(batchSpeclization);
+// // console.log(typeof batchSpeclization);
 
-// function hello(avar1,avar2){
-//     return avar1 + avar2;
+// // let allPresent = true; //block scope
+// // console.log(allPresent); 
+// // console.log(typeof allPresent);
+
+// // var isLegacy = 'Yes'; 
+// // console.log(isLegacy); // veriable with var get access thorught window object 
+// // console.log(typeof isLegacy); //variable will availabe gloablly in program
+
+
+
+
+// // function hello(avar1,avar2){
+// //     return avar1 + avar2;
+// // }
+
+// // console.log( hello("chethan","nagaraju"));
+// // console.log( hello(600,"nagaraju"));
+// // console.log( hello("chethan",600));
+// // console.log( hello(600,600));
+
+
+// // let newarray= [10,'chethan',20,'nagaraju'];
+// // console.log(newarray);
+
+// // console.log(newarray[2]='60')
+// // console.log(newarray)
+
+// // console.log(newarray[2]=60)
+
+
+// let personinfo = {
+//     name: 'chethan',
+//     age: '23',
+//     gender: 'male',
+//     address: {
+//         presentadd: {
+//             location: 'mysore',
+//             area: 'jp nagara',
+//             pincode: 571110,
+//             permanantadd: {
+//                 location: 'T Narasiputa',
+//                 area: 'patel street',
+//                 pincode: 571101,
+//             }
+
+
+//         }
+//     }
+// };
+// console.log(personinfo);
+// personinfo.address.presentadd.pincode
+// // 
+
+// function addition(a, b) {
+//     return a + b;
+// }
+// let add = addition(10, 50);
+// console.log(add)
+
+// // if else ststement
+
+
+// let letterA = 'a';
+// let letterB = 'a';
+
+// if (letterA == letterB) {
+//     console.log("it is true")
+// }
+// else {
+//     console.log("it is false")
 // }
 
-// console.log( hello("chethan","nagaraju"));
-// console.log( hello(600,"nagaraju"));
-// console.log( hello("chethan",600));
-// console.log( hello(600,600));
+// // arrow function 
+// let ad=(n1,n2)=>{
+//     return n1+n2;
+// }
+//  console.log(ad(30,70));
 
 
-// let newarray= [10,'chethan',20,'nagaraju'];
-// console.log(newarray);
 
-// console.log(newarray[2]='60')
-// console.log(newarray)
+// let leta = 10;
+// let letb = 'b';
 
-// console.log(newarray[2]=60)
-
-
-let personinfo = {
-    name: 'chethan',
-    age: '23',
-    gender: 'male',
-    address: {
-        presentadd: {
-            location: 'mysore',
-            area: 'jp nagara',
-            pincode: 571110,
-        permanantadd: {
-             location: 'T Narasiputa',
-             area: 'patel street',
-             pincode: 571101,
-            }
+// if (leta === letb) {
+//     console.log("it is true")
+// }
+// else {
+//     console.log("it is false")
+// }
 
 
-        }
-    }
+let myobj = {
+    name: 'chethan'
 };
-console.log(personinfo);
-personinfo.address.presentadd.pincode
+console.log(myobj)
+
+let message = 'movie';
+let single = message + 'hello\n' + 'world\n' + 'my\n' + 'name\n' + 'is\n' + 'chethan\n';
+console.log(single);
+
+let news=`${message} 'hello' 
+         'world'
+         'my'
+         'name'
+         'is'
+         'chethan'`;
+         console.log(news);
