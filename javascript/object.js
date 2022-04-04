7c0229c103713b040de5a0eee56eb0bd23891bb0
chethan <chethann744@gmail.com>
2022-04-04 09:39:34 +0530

    adding components

---
+++ javascript/object.js
@@ -0,0 +1,19 @@
+const car ={
+    name:'hyunday',
+    cost:'10000',
+    model:'2016'
+};
+
+let obj=Object.keys(car);
+console.log(obj)
+
+let objj=Object.values(car);
+console.log(objj)
+
+const car2={
+    name:'honda',
+    cost:'20000',
+    model:'2015'
+};
+let carobj=Object.assign(car,car2);
+console.log(carobj)
