e1613a2adf8ca07205f8567888580b50b4c6d1f6
chethan <chethann744@gmail.com>
2022-04-06 12:10:51 +0530

    adding learytica

---
+++ Learnlytica/src/app/components/course/course.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+  courseTitle = 'Git for begginer';
+  currentDate = new Date();
+
+  courseDetails = {
+    title:'ss',
+    price: 100
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
